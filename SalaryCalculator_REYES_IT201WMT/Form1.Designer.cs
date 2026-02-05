4ddb09b5679849ea9c90b84cd27c66390ef6b772
C204-31 <C204-31@C204-PC11>
2026-02-05 15:53:13 +0800

    Add project files.

---
+++ SalaryCalculator_REYES_IT201WMT/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SalaryCalculator_REYES_IT201WMT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(297, 43);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(302, 42);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Salary Calculator";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(300, 99);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(111, 20);
+            this.lblHours.TabIndex = 1;
+            this.lblHours.Text = "Hours Worked";
+            this.lblHours.Click += new System.EventHandler(this.lblHours_Click);
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRate.Location = new System.Drawing.Point(300, 131);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(93, 20);
+            this.lblRate.TabIndex = 2;
+            this.lblRate.Text = "Hourly Rate";
+            this.lblRate.Click += new System.EventHandler(this.lblRate_Click);
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalary.Location = new System.Drawing.Point(300, 162);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(100, 20);
+            this.lblSalary.TabIndex = 3;
+            this.lblSalary.Text = "Gross Salary";
+            this.lblSalary.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtHours
+            // 
+            this.txtHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHours.Location = new System.Drawing.Point(435, 93);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(154, 26);
+            this.txtHours.TabIndex = 4;
+            this.txtHours.TextChanged += new System.EventHandler(this.txtHours_TextChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalary.Location = new System.Drawing.Point(435, 159);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.ReadOnly = true;
+            this.txtSalary.Size = new System.Drawing.Size(154, 26);
+            this.txtSalary.TabIndex = 5;
+            this.txtSalary.TextChanged += new System.EventHandler(this.txtSalary_TextChanged);
+            // 
+            // txtRate
+            // 
+            this.txtRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRate.Location = new System.Drawing.Point(435, 125);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(154, 26);
+            this.txtRate.TabIndex = 6;
+            this.txtRate.TextChanged += new System.EventHandler(this.txtRate_TextChanged);
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(304, 200);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(75, 36);
+            this.btnCompute.TabIndex = 7;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(410, 200);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(514, 200);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 36);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SalaryCalculator_REYES_IT201WMT/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculator_REYES_IT201WMT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHours_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblRate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHours_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            double v1, v2;
+            double.TryParse(txtHours.Text, out v1);
+            double.TryParse(txtRate.Text, out v2);
+            
+            txtSalary.Text = (v1*v2*24).ToString();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHours.Clear();
+            txtRate.Clear();
+            txtSalary.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
